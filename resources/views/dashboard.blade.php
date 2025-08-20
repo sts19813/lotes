bb904a08730a8496186b13caf630ebaa7fb3885d
Jose Santos Dzul Chi <jose.15070017@itsmotul.edu.mx>
2025-08-19 22:00:47 -0600

    instalacion del metronic en laravel

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Mi Dashboard')
+
+@section('content')
+    <h1>Bienvenido a mi Dashboard</h1>
+    <div class="row">
+        <!-- Aquí copias secciones de index.html o dashboards de tu plantilla -->
+    </div>
+@endsection
